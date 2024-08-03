8ccd95e064ca60bddb9d28b81dcb9222d941961b
latinox33 <latinox33@gmail.com>
2024-08-04 01:19:04 +0200

    refactor(chatbox): remove old bubble message code

--- client/src/components/ChatBoxMessages.vue
+++ client/src/components/ChatBoxMessages.vue
@@ -12,38 +12,6 @@
         :key="idx"
       >
         <ChatBoxMessageBubble v-bind="message" />
-        <!--        <div-->
-        <!--          class="flex gap-1 justify-start mb-3"-->
-        <!--          :class="{-->
-        <!--            'flex-row-reverse items-end': message.author === 'user',-->
-        <!--          }"-->
-        <!--        >-->
-        <!--          <div-->
-        <!--            class="w-[40px] h-[40px] min-w-[40px] border-rd-full flex items-center justify-center"-->
-        <!--            :class="{-->
-        <!--              'bg-[#FFC333] text-[#0f0b0b]': message.author === 'user',-->
-        <!--              'bg-[#0f0b0b]': message.author === 'ai',-->
-        <!--            }"-->
-        <!--          >-->
-        <!--            {{ message.author === 'user' ? 'You' : 'AI' }}-->
-        <!--          </div>-->
-        <!--          <div-->
-        <!--            class="flex flex-col w-full max-w-[320px] leading-1.5 py-1 px-2 border-gray-200 bg-gray-100 dark:bg-gray-700"-->
-        <!--            :class="{-->
-        <!--              'rounded-e-xl rounded-es-xl': message.author === 'ai',-->
-        <!--              'rounded-s-xl rounded-se-xl': message.author === 'user',-->
-        <!--            }"-->
-        <!--          >-->
-        <!--            <div class="flex items-center space-x-2 rtl:space-x-reverse">-->
-        <!--              <span class="text-sm font-semibold text-gray-900 dark:text-gray-400">-->
-        <!--                <strong class="text-[#afabab] text-xs">{{ message.datePlaceholder }}</strong> {{ message.hour }}-->
-        <!--              </span>-->
-        <!--            </div>-->
-        <!--            <p class="text-sm font-normal m-0 py-1 text-gray-900 dark:text-white">-->
-        <!--              {{ message.message }}-->
-        <!--            </p>-->
-        <!--          </div>-->
-        <!--        </div>-->
       </div>
     </TransitionGroup>
   </div>
